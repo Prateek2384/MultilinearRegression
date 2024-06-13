64aef04bd7f0a274e12713837fa73f2d049a3a8c
Prateek Swami <118006355+Prateek2384@users.noreply.github.com>
2024-06-13 09:01:35 +0530

    Update multiple_linear_regression.py

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
-X = dataset.iloc[:, :-1].values
+x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 print(X)
 
@@ -15,17 +15,21 @@ print(X)
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
-print(X)
+x = np.array(ct.fit_transform(x))
+print(x)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+print(x_train)
+print(x_test)
+print(y_train)
+print(y_test)
 
 # Training the Multiple Linear Regression model on the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+regressor.fit(x_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
