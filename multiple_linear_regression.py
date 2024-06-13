f67cb42d0e054e16fb655c6cc0844d370fe5ac89
Prateek Swami <118006355+Prateek2384@users.noreply.github.com>
2024-06-13 09:02:53 +0530

    multiple_linear_regression.py

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -32,6 +32,6 @@ regressor = LinearRegression()
 regressor.fit(x_train, y_train)
 
 # Predicting the Test set results
-y_pred = regressor.predict(X_test)
+y_pred = regressor.predict(x_test)
 np.set_printoptions(precision=2)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
